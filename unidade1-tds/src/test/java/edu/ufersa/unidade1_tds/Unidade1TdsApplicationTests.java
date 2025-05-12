ab8641c5824442a624b30ecd0c75b8d77f2e6ae1
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-12 11:43:52 -0300

    Criando projeto no spring

---
+++ unidade1-tds/src/test/java/edu/ufersa/unidade1_tds/Unidade1TdsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ufersa.unidade1_tds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unidade1TdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

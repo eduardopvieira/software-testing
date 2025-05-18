5b4cd318f4338b0e105f407999ee642cc1e5c185
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-05-18 16:20:13 -0300

    refactor no nome do metodo do dinheiro e na logica do movimento

--- Unidade1/src/model/Duende.java
+++ Unidade1/src/model/Duende.java
@@ -4,37 +4,48 @@ import java.util.Random;
 
 public class Duende {
     private int id;
-    private long money;
+    private long coins;
     private double position;
+    private double minHorizon, maxHorizon; //precisam desses parametros pra limitar o movimento
 
-    public Duende(int id) {
+    public Duende(int id, double minHorizon, double maxHorizon) {
         this.id = id;
-        this.money = 1000000L;
+        this.coins = 1000000L;
         this.position = id * 0.1; // Posição inicial baseada no ID
+        this.minHorizon = minHorizon;
+        this.maxHorizon = maxHorizon;
+
     }
 
     public void move() {
         Random random = new Random();
         double posAntiga = getPosition();
         double movimento = random.nextDouble() * 2 - 1; // -1 a 1
+
+        double newPos = posAntiga + (movimento * (this.coins/100000));
         
-        double novaPosicao = Math.max(0, Math.min(30, posAntiga + movimento)); //delimita o movimento pra ficar entre 0 até 30
-        this.setPosition(novaPosicao);
+        if (newPos < minHorizon) {
+            newPos = minHorizon;
+        } else if (newPos > maxHorizon) {
+            newPos = maxHorizon;
+        }
+
+        this.setPosition(newPos);
         
         System.out.println("Duende " + this.getId() + " saiu de " + posAntiga + " para " + this.getPosition());
     }
 
-    private Long giveMoney() {
-        Long perdido = this.money / 2;
-        this.money = this.money - perdido;
+    private Long giveCoins() {
+        Long perdido = this.coins / 2;
+        this.coins = this.coins - perdido;
         System.out.println("O Duende " + id + " perdeu " + perdido + " dinheiros. Coitado.");
         return perdido;
     }
 
     public void steal(Duende victim) {
         if (victim != null && victim != this) {
-            Long roubado = victim.giveMoney();
-            this.money = this.money + roubado;
+            Long roubado = victim.giveCoins();
+            this.coins = this.coins + roubado;
             System.out.println("O Duende " + id + " roubou o Duende " + victim.getId() + " com sucesso.");
         }
     }
@@ -42,8 +53,8 @@ public class Duende {
     // Getters e Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-    public Long getMoney() { return money; }
-    public void setMoney(Long money) { this.money = money; }
+    public Long getCoins() { return coins; }
+    public void setCoins(Long coins) { this.coins = coins; }
     public double getPosition() { return position; }
     public void setPosition(double position) { this.position = position; }
 }

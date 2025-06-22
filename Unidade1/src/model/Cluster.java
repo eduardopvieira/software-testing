483b7c544437f7f302f8666f665003b86ca8db80
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-06-22 17:29:17 -0300

    feat: enhance Cluster class with quantity tracking and remove id references

--- Unidade1/src/model/Cluster.java
+++ Unidade1/src/model/Cluster.java
@@ -5,21 +5,22 @@ import model.interfaces.EntityOnHorizon;
 import java.util.List;
 
 public class Cluster implements EntityOnHorizon {
-    int id;
     List<Duende> duendes;
     long coins;
     double position;
+    int quantityDuendes;
 
-    public Cluster(int id, Duende duende1, Duende duende2) {
-        this.id = id;
+    public Cluster(Duende duende1, Duende duende2) {
         this.duendes = List.of(duende1, duende2);
         this.coins = duende1.getCoins() + duende2.getCoins();
         this.position = duende1.getPosition();
+        this.quantityDuendes = 2;
     }
 
     public void addToCluster(Duende duende) {
         duendes.add(duende);
         coins += duende.getCoins();
+        quantityDuendes++;
     }
 
 
@@ -34,14 +35,14 @@ public class Cluster implements EntityOnHorizon {
             throw new IllegalArgumentException("A posição não pode ser negativa.");
         }
         this.position = position;
-        System.out.println("Cluster " + id + " agora está na posição " + position);
+        System.out.println("Cluster agora está na posição " + position);
     }
 
     @Override
     public long beingStealed() {
         long perdido = this.coins / 2;
         this.coins -= perdido;
-        System.out.println("O Cluster " + id + " perdeu " + perdido + " moedas. Coitado.");
+        System.out.println("O Cluster perdeu " + perdido + " moedas. Coitado.");
         return perdido;
     }
 
@@ -58,7 +59,7 @@ public class Cluster implements EntityOnHorizon {
 
     @Override
     public int getId() {
-        return this.id;
+        return 0;
     }
 
     @Override
@@ -66,12 +67,19 @@ public class Cluster implements EntityOnHorizon {
         return this.coins;
     }
 
+    public int getQuantityDuendes() {
+        return this.quantityDuendes;
+    }
+
+    public List<Duende> getDuendes() {
+        return this.duendes;
+    }
     @Override
     public void addCoins(long amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("A quantidade de moedas a adicionar não pode ser negativa.");
         }
         this.coins += amount;
-        System.out.println("Cluster " + id + " agora tem " + coins + " moedas.");
+        System.out.println("Cluster agora tem " + coins + " moedas.");
     }
 }

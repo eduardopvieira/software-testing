8f100c466b0231c7f36f05df2ae4556f96283757
eduardopvieira <eduardoopazvieira@gmail.com>
2025-06-27 09:40:30 -0300

    criação do banco de dados na main

--- Unidade1/src/Main.java
+++ Unidade1/src/Main.java
@@ -1,7 +1,12 @@
-import view.MenuView;
+import model.dao.DatabaseManager;
+import view.LoginView;
 
 public class Main {
     public static void main(String[] args) {
-        MenuView.criarJanelaInput();
+        // pd deixar chamar isso toda a vez, o comando só cria a tabela caso n exista
+        DatabaseManager.criarTabelaUsuarios();
+
+        LoginView menu = new LoginView();
+        menu.exibir();
     }
 }
